8dd257632b634190c305403558a88330e5cb4b87
Gideon Ngetich <115392447+Gideon-Ngetich@users.noreply.github.com>
2023-10-05 20:57:01 +0000

    Complete home page design

--- main-guitar-app/src/components/Home.js
+++ main-guitar-app/src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
+
 function Home() {
-  const background = `bg-[url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_48742911_happy-man-jumping-with-guitar-in-sunset-time.html%3Fvti%3Dn4b8468scqew0bsn98-1-72%26is_plus%3D1&psig=AOvVaw0utH_u_khGDA5gd8m-O90s&ust=1696614486045000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPCorK-_34EDFQAAAAAdAAAAABAZ")]`;
+  const background = `bg-[url("/workspaces/Guitar-Tuning-WebApp/main-guitar-app/src/assets/guitar.jpg")]`;
   const features = [
     {
       title: 'Easy to use',
@@ -16,6 +17,43 @@ function Home() {
       description: 'AjayTuner allows you to customize the tuning settings to match your instrument and playing style. You can also save your settings for future use.',
     },
   ];
+
+  const tutorials = [
+    {
+      title: 'Getting started with AjayTuner',
+      description: 'Learn how to use AjayTuner to tune your guitar in this beginner-friendly tutorial.',
+      url: 'https://www.youtube.com/watch?v=abc123',
+    },
+    {
+      title: 'Advanced tuning techniques',
+      description: 'Take your tuning skills to the next level with these advanced techniques and tips.',
+      url: 'https://www.youtube.com/watch?v=def456',
+    },
+    {
+      title: 'Troubleshooting common tuning problems',
+      description: 'Learn how to troubleshoot common tuning problems and get your guitar sounding great.',
+      url: 'https://www.youtube.com/watch?v=ghi789',
+    },
+  ];
+
+  const jammingSessions = [
+    {
+      title: 'Jamming with the band',
+      description: 'Watch this video of our band jamming together and get inspired to create your own music.',
+      url: 'https://www.youtube.com/watch?v=abc123',
+    },
+    {
+      title: 'Improvisation techniques',
+      description: 'Learn how to improvise on your instrument with these tips and tricks from our expert musicians.',
+      url: 'https://www.youtube.com/watch?v=def456',
+    },
+    {
+      title: 'Collaborating with other musicians',
+      description: 'Discover the joys of collaborating with other musicians and learn how to make beautiful music together.',
+      url: 'https://www.youtube.com/watch?v=ghi789',
+    },
+  ];
+
   const myImage = `https://media.istockphoto.com/id/510638249/photo/tuning-a-guitar.jpg?s=612x612&w=0&k=20&c=qNzNrOWItwYGEd_6QppzQZ2gQ99LIQfs0wa6VW5fxPA=`;
   return (
 
@@ -54,19 +92,20 @@ function Home() {
         </div>
       </nav>
 
-      <div class={`${background} bg-cover bg-center z-50`}>
-          <video autoplay muted loop>
-            <source src="Video by Ibrahim Bennett from Pexels: https://www.pexels.com/video/kuala-lumpur-night-to-day-timelapse-18522098/" type="video/mp4"></source>
-              Your browser does not support the video tag.
-          </video>
-          <div class="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-            <div class="text-center">
-              <h2 class="text-base font-semibold text-blue-600 tracking-wide uppercase">AjayTuner</h2>
-              <p class="mt-1 text-4xl font-extrabold text-black-900 dark:text-black sm:text-5xl sm:tracking-tight lg:text-6xl">Tune your guitar</p>
-              <p class="max-w-xl mt-5 mx-auto text-xl text-gray-900 dark:text-black-400">AjayTuner is a web app that helps you tune your guitar. It uses the microphone of your device to detect the frequency of the string you are playing and tells you whether the string is in tune or not.</p>
-            </div>
+      <div class={`${background} bg-cover bg-center h-screen bg-opacity-50`}>
+        <div class="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div class="text-center">
+            <h2 class="text-2xl font-semibold text-blue-600 tracking-wide uppercase">AjayTuner</h2>
+            <p class="mt-1 text-4xl font-extrabold text-black-900 dark:text-yellow-600 sm:text-5xl sm:tracking-tight lg:text-6xl">Tune your guitar</p>
+            <p class="max-w-xl mt-5 mx-auto text-2xl text-white dark:text-white">AjayTuner is a web app that helps you tune your guitar. It uses the microphone of your device to detect the frequency of the string you are playing and tells you whether the string is in tune or not.</p>
           </div>
         </div>
+        <div class="mt-8">
+              <a href="/tutorials" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Log In
+              </a>
+            </div>
+      </div>
 
       <section class="bg-white dark:bg-gray-900">
         <div class="bg-white dark:bg-gray-900">
@@ -93,6 +132,85 @@ function Home() {
         </div>
       </section>
 
+      <section class="bg-blue-200">
+        <div class="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div class="text-center">
+            <h2 class="text-3xl font-extrabold text-black sm:text-4xl">
+              Watch our tutorials
+            </h2>
+            <div class="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
+              {tutorials.map((tutorial) => (
+                <div key={tutorial.title} class="bg-white dark:bg-gray-900 rounded-lg shadow-md overflow-hidden">
+                  <a href={tutorial.url} target="_blank" rel="noopener noreferrer">
+                    <div class="p-6">
+                      <h3 class="text-lg font-bold text-gray-900 dark:text-white">{tutorial.title}</h3>
+                      <p class="mt-2 text-gray-500 dark:text-gray-400">{tutorial.description}</p>
+                    </div>
+                  </a>
+                </div>
+              ))}
+            </div>
+            <div class="mt-8">
+              <a href="/tutorials" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                View all tutorials
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    <section class="dark:bg-gray-900">
+      <div class="max-w-screen-xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <h2 class="text-3xl font-extrabold text-white sm:text-4xl">
+            Jamming sessions
+          </h2>
+          <div class="text-black-900 mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {jammingSessions.map((session) => (
+              <div key={session.title} class="bg-white dark:bg-white text-black-900 rounded-lg shadow-md overflow-hidden">
+                <a href={session.url} target="_blank" rel="noopener noreferrer">
+                  <div class="p-6">
+                    <h3 class="text-lg font-bold text-black-900 dark:text-black">{session.title}</h3>
+                    <p class="mt-2 text-black-500 dark:text-black-400">{session.description}</p>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+          <div class="mt-8">
+            <a href="/jamming-sessions" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              View all jamming sessions
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="bg-gray-800">
+        <div class="max-w-screen-xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:px-8">
+          <div class="flex justify-center space-x-6">
+            <a href="#" class="text-gray-400 hover:text-gray-300">
+              <span class="sr-only">Facebook</span>
+              <i class="fab fa-facebook fa-2x"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300">
+              <span class="sr-only">Twitter</span>
+              <i class="fab fa-twitter fa-2x"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300">
+              <span class="sr-only">Instagram</span>
+              <i class="fab fa-instagram fa-2x"></i>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300">
+              <span class="sr-only">GitHub</span>
+              <i class="fab fa-github fa-2x"></i>
+            </a>
+          </div>
+          <div class="mt-8 text-center text-gray-400">
+            <p>&copy; 2023 AjayTuner. All rights reserved.</p>
+          </div>
+        </div>
+      </footer>
     </>
 
   )
